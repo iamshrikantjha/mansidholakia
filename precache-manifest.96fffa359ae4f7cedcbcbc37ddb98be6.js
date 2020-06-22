a47e084245b4a2d9587c2c9d0ef4f9439d3a5e85
iamshrikantjha <shrikantjha11@gmail.com>
2020-06-22 21:19:12 +0530

    Updates

---
+++ precache-manifest.96fffa359ae4f7cedcbcbc37ddb98be6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66c03bdc735194c657da8c54277cb39",
+    "url": "/mansidholakia/index.html"
+  },
+  {
+    "revision": "1569ff86f026daa58d51",
+    "url": "/mansidholakia/static/css/2.1f5dcd6f.chunk.css"
+  },
+  {
+    "revision": "ade0c4bdf483dc3b8fa6",
+    "url": "/mansidholakia/static/css/main.9c4b7de1.chunk.css"
+  },
+  {
+    "revision": "1569ff86f026daa58d51",
+    "url": "/mansidholakia/static/js/2.48e3069c.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/mansidholakia/static/js/2.48e3069c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade0c4bdf483dc3b8fa6",
+    "url": "/mansidholakia/static/js/main.f55da182.chunk.js"
+  },
+  {
+    "revision": "5c3634b6376fd6def8c4",
+    "url": "/mansidholakia/static/js/runtime-main.25065686.js"
+  },
+  {
+    "revision": "7270157832de316f01bae75f4bae08e0",
+    "url": "/mansidholakia/static/media/blog_1.72701578.png"
+  },
+  {
+    "revision": "9adedbce672a056a96d237918ca72512",
+    "url": "/mansidholakia/static/media/blog_2.9adedbce.png"
+  },
+  {
+    "revision": "4cf3e7b18b96d9a6aef6de4ea24f3c2c",
+    "url": "/mansidholakia/static/media/blog_3.4cf3e7b1.png"
+  },
+  {
+    "revision": "058d07bb91585728bc596d100dda789e",
+    "url": "/mansidholakia/static/media/blog_4.058d07bb.png"
+  },
+  {
+    "revision": "f4683e36f83c41ea5ae262d16a2577d7",
+    "url": "/mansidholakia/static/media/blog_5.f4683e36.png"
+  },
+  {
+    "revision": "238c1d4604d9b97d1111800a1c5853cf",
+    "url": "/mansidholakia/static/media/darren.238c1d46.jpg"
+  },
+  {
+    "revision": "c7fac338330d8e52ff463fae9ca66547",
+    "url": "/mansidholakia/static/media/hero.c7fac338.jpg"
+  },
+  {
+    "revision": "e3a95aedd29feccf044ab239642d2246",
+    "url": "/mansidholakia/static/media/hero3.e3a95aed.jpg"
+  }
+]);
